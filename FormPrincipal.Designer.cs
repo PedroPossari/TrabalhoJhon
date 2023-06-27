85e09de90e256464930b15347cb4514f1b675e6e
Vidal-Victor <victorgabrielvidal727@gmail.com>
2023-06-27 20:05:35 -0300

    Telinha principal

--- FormPrincipal.Designer.cs
+++ FormPrincipal.Designer.cs
@@ -28,125 +28,245 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            reg_venda = new Label();
-            Esto = new Label();
-            AddProduto = new Label();
-            H_venda = new Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            USer = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label5 = new Label();
+            label3 = new Label();
+            label2 = new Label();
             CadCliente = new Label();
             CadUser = new Label();
+            H_venda = new Label();
+            Esto = new Label();
+            reg_venda = new Label();
+            label1 = new Label();
+            label4 = new Label();
+            AddProduto = new Label();
+            label8 = new Label();
             SuspendLayout();
             // 
-            // label1
+            // USer
             // 
-            label1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(322, 9);
-            label1.Margin = new Padding(4, 0, 4, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(220, 74);
-            label1.TabIndex = 5;
-            label1.Text = "      BEM VIND@!\r\nO que deseja fazer?";
+            USer.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            USer.Location = new Point(413, 12);
+            USer.Margin = new Padding(4, 0, 4, 0);
+            USer.Name = "USer";
+            USer.Size = new Size(383, 22);
+            USer.TabIndex = 35;
+            USer.Text = "AAA";
             // 
-            // reg_venda
+            // label6
             // 
-            reg_venda.BorderStyle = BorderStyle.FixedSingle;
-            reg_venda.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            reg_venda.Location = new Point(53, 111);
-            reg_venda.Margin = new Padding(4, 0, 4, 0);
-            reg_venda.Name = "reg_venda";
-            reg_venda.Padding = new Padding(28, 35, 0, 0);
-            reg_venda.Size = new Size(170, 97);
-            reg_venda.TabIndex = 8;
-            reg_venda.Text = "Registrar Venda";
-            reg_venda.UseWaitCursor = true;
-            reg_venda.Click += label2_Click;
+            label6.Cursor = Cursors.Hand;
+            label6.Image = (Image)resources.GetObject("label6.Image");
+            label6.Location = new Point(88, 313);
+            label6.Name = "label6";
+            label6.Size = new Size(95, 54);
+            label6.TabIndex = 33;
+            label6.Click += H_venda_Click;
             // 
-            // Esto
+            // label7
             // 
-            Esto.BorderStyle = BorderStyle.FixedSingle;
-            Esto.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            Esto.Location = new Point(339, 263);
-            Esto.Margin = new Padding(4, 0, 4, 0);
-            Esto.Name = "Esto";
-            Esto.Padding = new Padding(50, 35, 0, 0);
-            Esto.Size = new Size(170, 97);
-            Esto.TabIndex = 7;
-            Esto.Text = "Estoque";
+            label7.Cursor = Cursors.Hand;
+            label7.Image = (Image)resources.GetObject("label7.Image");
+            label7.Location = new Point(662, 313);
+            label7.Name = "label7";
+            label7.Size = new Size(95, 54);
+            label7.TabIndex = 32;
+            label7.Click += CadUser_Click;
             // 
-            // AddProduto
+            // label5
             // 
-            AddProduto.BorderStyle = BorderStyle.FixedSingle;
-            AddProduto.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            AddProduto.Location = new Point(339, 111);
-            AddProduto.Margin = new Padding(4, 0, 4, 0);
-            AddProduto.Name = "AddProduto";
-            AddProduto.Padding = new Padding(23, 35, 0, 0);
-            AddProduto.Size = new Size(170, 97);
-            AddProduto.TabIndex = 8;
-            AddProduto.Text = "Adicionar Produto";
+            label5.Cursor = Cursors.Hand;
+            label5.Image = (Image)resources.GetObject("label5.Image");
+            label5.Location = new Point(373, 313);
+            label5.Name = "label5";
+            label5.Size = new Size(95, 54);
+            label5.TabIndex = 31;
+            label5.Click += Esto_Click;
             // 
-            // H_venda
+            // label3
             // 
-            H_venda.BorderStyle = BorderStyle.FixedSingle;
-            H_venda.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            H_venda.Location = new Point(53, 263);
-            H_venda.Margin = new Padding(4, 0, 4, 0);
-            H_venda.Name = "H_venda";
-            H_venda.Padding = new Padding(13, 35, 0, 0);
-            H_venda.Size = new Size(170, 97);
-            H_venda.TabIndex = 9;
-            H_venda.Text = "Histórico de Vendas";
+            label3.Cursor = Cursors.Hand;
+            label3.Image = (Image)resources.GetObject("label3.Image");
+            label3.Location = new Point(661, 161);
+            label3.Name = "label3";
+            label3.Size = new Size(95, 54);
+            label3.TabIndex = 29;
+            label3.Click += CadCliente_Click;
+            // 
+            // label2
+            // 
+            label2.Cursor = Cursors.Hand;
+            label2.Image = (Image)resources.GetObject("label2.Image");
+            label2.Location = new Point(86, 161);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 54);
+            label2.TabIndex = 28;
+            label2.Click += label2_Click;
             // 
             // CadCliente
             // 
             CadCliente.BorderStyle = BorderStyle.FixedSingle;
-            CadCliente.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            CadCliente.Location = new Point(625, 111);
+            CadCliente.Cursor = Cursors.Hand;
+            CadCliente.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            CadCliente.Location = new Point(622, 124);
             CadCliente.Margin = new Padding(4, 0, 4, 0);
             CadCliente.Name = "CadCliente";
-            CadCliente.Padding = new Padding(25, 35, 0, 0);
+            CadCliente.Padding = new Padding(15, 5, 0, 0);
             CadCliente.Size = new Size(170, 97);
-            CadCliente.TabIndex = 11;
+            CadCliente.TabIndex = 27;
             CadCliente.Text = "Cadastrar Cliente";
+            CadCliente.Click += CadCliente_Click;
             // 
             // CadUser
             // 
             CadUser.BorderStyle = BorderStyle.FixedSingle;
-            CadUser.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            CadUser.Location = new Point(625, 263);
+            CadUser.Cursor = Cursors.Hand;
+            CadUser.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            CadUser.Location = new Point(622, 276);
             CadUser.Margin = new Padding(4, 0, 4, 0);
             CadUser.Name = "CadUser";
-            CadUser.Padding = new Padding(20, 35, 0, 0);
+            CadUser.Padding = new Padding(10, 5, 0, 0);
             CadUser.Size = new Size(170, 97);
-            CadUser.TabIndex = 10;
+            CadUser.TabIndex = 26;
             CadUser.Text = "Cadastrar Usuário";
+            CadUser.Click += CadUser_Click;
+            // 
+            // H_venda
+            // 
+            H_venda.BorderStyle = BorderStyle.FixedSingle;
+            H_venda.Cursor = Cursors.Hand;
+            H_venda.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            H_venda.Location = new Point(50, 276);
+            H_venda.Margin = new Padding(4, 0, 4, 0);
+            H_venda.Name = "H_venda";
+            H_venda.Padding = new Padding(10, 5, 0, 0);
+            H_venda.Size = new Size(170, 97);
+            H_venda.TabIndex = 25;
+            H_venda.Text = "Histórico de Vendas";
+            H_venda.Click += H_venda_Click;
+            // 
+            // Esto
+            // 
+            Esto.BorderStyle = BorderStyle.FixedSingle;
+            Esto.Cursor = Cursors.Hand;
+            Esto.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Esto.Location = new Point(336, 276);
+            Esto.Margin = new Padding(4, 0, 4, 0);
+            Esto.Name = "Esto";
+            Esto.Padding = new Padding(50, 5, 0, 0);
+            Esto.Size = new Size(170, 97);
+            Esto.TabIndex = 22;
+            Esto.Text = "Estoque";
+            Esto.Click += Esto_Click;
+            // 
+            // reg_venda
+            // 
+            reg_venda.BorderStyle = BorderStyle.FixedSingle;
+            reg_venda.Cursor = Cursors.Hand;
+            reg_venda.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            reg_venda.ImageAlign = ContentAlignment.BottomLeft;
+            reg_venda.Location = new Point(50, 124);
+            reg_venda.Margin = new Padding(4, 0, 4, 0);
+            reg_venda.Name = "reg_venda";
+            reg_venda.Padding = new Padding(20, 5, 0, 0);
+            reg_venda.Size = new Size(170, 97);
+            reg_venda.TabIndex = 23;
+            reg_venda.Text = "Registrar Venda";
+            reg_venda.UseWaitCursor = true;
+            reg_venda.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(319, 12);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(175, 45);
+            label1.TabIndex = 21;
+            label1.Text = "Bem Vindo\r\nO que deseja fazer?";
+            // 
+            // label4
+            // 
+            label4.Cursor = Cursors.Hand;
+            label4.Image = (Image)resources.GetObject("label4.Image");
+            label4.Location = new Point(373, 161);
+            label4.Name = "label4";
+            label4.Size = new Size(95, 54);
+            label4.TabIndex = 30;
+            label4.Click += AddProduto_Click;
+            // 
+            // AddProduto
+            // 
+            AddProduto.BackColor = Color.Transparent;
+            AddProduto.BorderStyle = BorderStyle.FixedSingle;
+            AddProduto.Cursor = Cursors.Hand;
+            AddProduto.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            AddProduto.Location = new Point(336, 124);
+            AddProduto.Margin = new Padding(4, 0, 4, 0);
+            AddProduto.Name = "AddProduto";
+            AddProduto.Padding = new Padding(15, 5, 0, 0);
+            AddProduto.Size = new Size(170, 97);
+            AddProduto.TabIndex = 24;
+            AddProduto.Text = "Adicionar Produto";
+            AddProduto.Click += AddProduto_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(182, 161);
+            label8.Name = "label8";
+            label8.Size = new Size(38, 15);
+            label8.TabIndex = 34;
+            label8.Text = "label8";
+            label8.Visible = false;
             // 
             // FormPrincipal
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(846, 415);
+            Controls.Add(USer);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(CadCliente);
             Controls.Add(CadUser);
             Controls.Add(H_venda);
-            Controls.Add(AddProduto);
             Controls.Add(Esto);
             Controls.Add(reg_venda);
             Controls.Add(label1);
+            Controls.Add(label4);
+            Controls.Add(AddProduto);
+            Controls.Add(label8);
             Margin = new Padding(4, 3, 4, 3);
             Name = "FormPrincipal";
             Text = "Main";
             FormClosing += Main_FormClosing;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label reg_venda;
-        private System.Windows.Forms.Label Esto;
-        private System.Windows.Forms.Label AddProduto;
-        private System.Windows.Forms.Label H_venda;
-        private System.Windows.Forms.Label CadCliente;
-        private System.Windows.Forms.Label CadUser;
+
+        private Label USer;
+        private Label label6;
+        private Label label7;
+        private Label label5;
+        private Label label3;
+        private Label label2;
+        private Label CadCliente;
+        private Label CadUser;
+        private Label H_venda;
+        private Label Esto;
+        private Label reg_venda;
+        private Label label1;
+        private Label label4;
+        private Label AddProduto;
+        private Label label8;
     }
 }
--- FormPrincipal.cs
+++ FormPrincipal.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TrabalhoLipa.Models;
 
 namespace TrabalhoLipa
 {
@@ -19,13 +20,51 @@ namespace TrabalhoLipa
         private void label2_Click(object sender, EventArgs e)
         {
             Registar_Venda venda = new Registar_Venda();
-            venda.Show();
             this.Hide();
+            venda.ShowDialog();
+            this.Show();
         }
 
+        private void AddProduto_Click(object sender, EventArgs e)
+        {
+            Cadastrar_Produto cadastro = new Cadastrar_Produto();
+            this.Hide();
+            cadastro.ShowDialog();
+            this.Show();
+        }
         private void Main_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void CadCliente_Click(object sender, EventArgs e)
+        {
+            Cadastrar_Cliente cliente = new Cadastrar_Cliente();
+            this.Hide();
+            cliente.ShowDialog();
+            this.Show();
+        }
+
+        private void CadUser_Click(object sender, EventArgs e)
+        {
+            Cadastro cad = new Cadastro();
+            cad.Show();
+        }
+
+        private void Esto_Click(object sender, EventArgs e)
+        {
+            Estoquecs estoque = new Estoquecs();
+            this.Hide();
+            estoque.ShowDialog();
+            this.Show();
+        }
+
+        private void H_venda_Click(object sender, EventArgs e)
+        {
+            Historico_de_Vendas venda = new Historico_de_Vendas();
+            this.Hide();
+            venda.ShowDialog();
+            this.Show();
         }
     }
 }
